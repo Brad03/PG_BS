ecf216b6bf68be3e99d05f90772f2c5374b48321
Brad03 <33519490+Brad03@users.noreply.github.com>
2018-01-11 09:04:54 -0500

    Add files via upload

---
+++ Pg_BS1.py
@@ -0,0 +1,13 @@
+import pyautogui as pg
+import time
+
+pg.hotkey('ctrl', 'winleft', 'd')
+pg.hotkey('winleft')
+pg.typewrite('chrome\n', 0.1)
+time.sleep(2)
+pg.hotkey('winleft','up')
+pg.typewrite('youtube.com\n')
+time.sleep(2.5)
+pg.typewrite('daily fornite\n')
+time.sleep(1.5)
+pg.click (379, 336)
